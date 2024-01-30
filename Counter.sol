d65cb19604a9582790843d3922ae3518e1b316ad
Meltem <107643674+ckrmeltem@users.noreply.github.com>
2024-01-30 11:28:13 +0300

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -37,4 +37,8 @@ contract Example {
     function get_description() external view returns (string memory) {
         return counter.description;
     }
+    function create(string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
+}
 } 
