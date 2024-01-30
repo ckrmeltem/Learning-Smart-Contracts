5d8987ac6db03e5ef5c60ccc65a186b4f6cb2378
Meltem <107643674+ckrmeltem@users.noreply.github.com>
2024-01-30 12:33:09 +0300

    Update Proposal.sol

--- Proposal.sol
+++ Proposal.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 
 contract ProposalContract {
+    address private owner; 
     uint256 private counter; // This line is added
     struct Proposal {
         string description; // Description of the proposal
@@ -16,6 +17,17 @@ contract ProposalContract {
 
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+
+   // Constructor to set the owner of the contract
+    constructor() {
+        owner = msg.sender;
+    }
+// Modifier to check if the caller is the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
 function createProposal(string calldata_title, string calldata_description, uint256_total_vote_to_end) external {
 counter +=1;
 proposal_history[counter] = Proposal(_title, _description 0,0,0, _total_vote_end, false, true
