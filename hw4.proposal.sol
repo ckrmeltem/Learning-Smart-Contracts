6beafba145c759c7477d1aa89263ab414289353b
Meltem <107643674+ckrmeltem@users.noreply.github.com>
2024-02-02 15:53:35 +0300

    Update hw4.proposal.sol

--- hw4.proposal.sol
+++ hw4.proposal.sol
@@ -2,32 +2,37 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
-address owner;
+    address private owner;
+    uint256 private counter;
 
-
-  
-    uint256 private counter; // This line is added
-    struct Proposal {
-        string title
-        string description; // Description of the proposal
-        uint256 approve; // Number of approve votes
-        uint256 reject; // Number of reject votes
-        uint256 pass; // Number of pass votes
-        uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
-        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
-        bool is_active; // This shows if others can vote to our contract
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 
-    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+    struct Proposal {
+        string title;
+        string description; // Description of the proposal
+        uint256 approve;
+        uint256 reject;
+        uint256 pass;
+        uint256 total_vote_to_end;
+        bool current_state;
+        bool is_active;
+    }
 
-  function create(string calldata _title,
-  string calldata _description,
-    uint256 _approve,
-    uint256 _reject,
-    uint256 _pass,
-    uint256 _total_vote_to_end) {
-    counter +=1;
-    proposal_history[counter] = Proposal(_title,_description, _approve,_reject, _pass, _total_vote_to_end, false,true);
-  
-}
+    mapping(uint256 => Proposal) proposal_history;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function create(string calldata _title, string calldata _description, uint256 _approve, uint256 _reject, uint256 _pass, uint256 _total_vote_to_end) external onlyOwner {
+        counter += 1;
+        proposal_history[counter] = Proposal(_title, _description, _approve, _reject, _pass, _total_vote_to_end, false, true);
+    }
+
+    function setOwner(address new_owner) external onlyOwner {
+        owner = new_owner;
+    }
 }
