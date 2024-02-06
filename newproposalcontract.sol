2133085062bd1646cec37b7942056eaf1e67f77e
Meltem <107643674+ckrmeltem@users.noreply.github.com>
2024-02-06 13:53:49 +0300

    Update newproposalcontract.sol

--- newproposalcontract.sol
+++ newproposalcontract.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+
 contract NewProposalContract {
     uint256 private _storedData;
     bool private _isActive;
     address private _owner;
     mapping(address => bool) private _hasVoted;
-
-    event ValueChanged(uint256 indexed oldValue, uint256 indexed newValue);
-    event Voted(address indexed voter);
-    event OwnerChanged(address indexed oldOwner, address indexed newOwner);
 
     modifier onlyOwner() {
         require(msg.sender == _owner, "Only owner can call this function");
@@ -28,7 +25,7 @@ contract NewProposalContract {
 
     constructor() {
         _owner = msg.sender;
-        _isActive = true; // You can set this to false initially if you want the contract to start inactive
+        _isActive = true;
     }
 
     function get() public view returns (uint256) {
@@ -36,9 +33,7 @@ contract NewProposalContract {
     }
 
     function set(uint256 newValue) public onlyOwner active {
-        uint256 oldValue = _storedData;
         _storedData = newValue;
-        emit ValueChanged(oldValue, newValue);
     }
 
     function toggleActivation() public onlyOwner {
@@ -47,14 +42,11 @@ contract NewProposalContract {
 
     function vote() public active newVoter {
         _hasVoted[msg.sender] = true;
-        emit Voted(msg.sender);
     }
 
     function setOwner(address newOwner) public onlyOwner {
         require(newOwner != address(0), "Invalid new owner address");
-        address oldOwner = _owner;
         _owner = newOwner;
-        emit OwnerChanged(oldOwner, newOwner);
     }
 
     function calculateCurrentState() public view returns (string memory) {
